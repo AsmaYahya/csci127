549e1b6fb0aaf0d8f9328cb08aefc2d308b150ae
AsmaYahya <102313654+AsmaYahya@users.noreply.github.com>
2022-04-04 22:20:07 -0400

    Update months.py

--- months.py
+++ months.py
@@ -1,7 +1,7 @@
 #CSci 127 Teaching Staff
 #October 2017
 #A program that uses functions to print out months.
-#Modified by:  ADD YOUR NAME HERE
+#Modified by:  Asma Yahya
 
 def monthString(monthNum):
      """
@@ -12,16 +12,40 @@ def monthString(monthNum):
      """
      
      monthString = ""
-
+     if monthNum == 1:
+          monthString = "January"
+     elif monthNum == 2:
+          monthString = "February"
+     elif monthNum == 3:
+          monthString = "March"
+     elif monthNum == 4:
+          monthString = "April"
+     elif monthNum == 5:
+          monthString = "May"
+     elif monthNum == 6:
+          monthString = "June"
+     elif monthNum == 7:
+          monthString = "July"
+     elif monthNum == 8:
+          monthString = "August"
+     elif monthNum == 9:
+          monthString = "September"
+     elif monthNum == 10:
+          monthString = "October"
+     elif monthNum == 11:
+          monthString = "November"
+     elif monthNum == 12:
+          monthString = "December"
+     else:
+          monthString = "Not yet Defined"
+          return(monthString)
+     
      ###################################
      ### FILL IN YOUR CODE HERE      ###
      ### Other than your name above, ###
      ### this is the only section    ###
      ### you change in this program. ###
      ###################################
-
-     return(monthString)
-
 
 
 def main():
